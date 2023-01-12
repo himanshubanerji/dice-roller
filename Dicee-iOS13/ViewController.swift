3c0bc748b210f8019927cb01bfc9739000375e73
Himanshu Banerji <himanshubanerji@gmail.com>
2023-01-13 00:47:05 +0530

    necessary files added for the app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    // IBOutlet used to reference a UI element
+    // here both the dice images are referenced
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    // storing all UI images in an UIImage array
+    let diceLogo: [UIImage] = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    var leftDiceNumber = 0
+    var rightDiceNumber = 0
+    
+    
+    // IBAction = Interface Builder Action
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        leftDiceNumber = Int.random(in: 0...5)
+        rightDiceNumber = Int.random(in: 0...5)
+        
+        
+        //        diceImageViewOne.image = diceLogo[leftDiceNumber]
+        diceImageViewOne.image = diceLogo.randomElement()
+        diceImageViewTwo.image = diceLogo[rightDiceNumber]
+        
+        
+        
+//        if(leftDiceNumber == 5) {
+//            leftDiceNumber = -1;
+//            rightDiceNumber = 6;
+//        }
+        
     }
-
-
+    
 }
 
